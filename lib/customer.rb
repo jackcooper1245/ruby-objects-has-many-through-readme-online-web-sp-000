e6339ccb93fc9c4b4c046a12ed88df8269c2fd97
Jack Cooper <jackcooper1245@gmail.com>
2020-04-21 10:30:08 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,9 +2,17 @@ class Customer
 
 attr_reader :name, :age
 
+@@all = []
+
 def initialize(name, age)
   @name = name
-  @age - age
+  @age = age
+  @@all << self
 end
+
+def self.all
+  @@all
+end
+
 
 end
